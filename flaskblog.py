d7a82144c1b45fee95b70f3c36aeb88d3baa6b89
louweyer-code <louisa.weyer@gmail.com>
2023-03-19 16:38:45 +0100

    fixed css connection problem

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,9 @@
-from crypt import methods
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '4ec5c3d89dec24ac7b637284e35e2e20'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
@@ -31,6 +31,7 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -39,10 +40,18 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
