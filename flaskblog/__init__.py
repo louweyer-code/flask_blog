57f5e2476e4b296a23d65852d5c433c41af2df72
louweyer-code <louisa.weyer@gmail.com>
2023-04-12 00:43:37 +0200

    attempt 8

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -8,6 +8,8 @@ from dotenv import load_dotenv
  
 load_dotenv()
 
+db = SQLAlchemy(app)
+
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
