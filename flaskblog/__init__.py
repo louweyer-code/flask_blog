84ced2b05b34ac016fe69387df215a89a980e1d5
louweyer-code <louisa.weyer@gmail.com>
2023-04-11 15:34:08 +0200

    attempt 2

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,12 +3,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
+from os import environ
  
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1234'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
